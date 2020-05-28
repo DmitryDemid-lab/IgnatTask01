c16790d67a030dfd16f58969368b77b04b591b1d
DmitryDemid <dimon.demid15@gmail.com>
2020-05-28 19:57:14 -0400

    display initials + Message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MyInitialsOutput from "./Components/MyInitialsOutput/MyInitialsOutput";
+import Post from "./Components/TelegramMessage/TelegramMessage";
 
 function App() {
   return (
     <div className="App">
       <MyInitialsOutput/>
+      <Post/>
     </div>
 
   );
---
+++ src/Components/TelegramMessage/TelegramMessage.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import s from './TelegramMessage.module.css';
+
+const Post = () => {
+    return (
+        <div className={s.post}>
+            <Avatar/>
+            <Message name={"Dmitry"} text={"Hi, how'r u doing?"}/>
+        </div>
+    )
+};
+
+const Avatar = () => {
+    return (
+        <img className={s.avatarIcon} src="https://avatarfiles.alphacoders.com/855/85557.png" alt="avatar"/>
+    )
+};
+
+type MessagePropsTypes = {
+    name: string,
+    text: string,
+}
+
+const Message = (props: MessagePropsTypes) => {
+    let date = new Date;
+    let hours = date.toLocaleTimeString();
+    return(
+        <div className={s.message}>
+            <div className={s.nameOfWriter}>
+                {props.name}
+            </div>
+            <div className={s.textMessage}>
+                {props.text}
+            </div>
+            <div className={s.sentTime}>
+                {hours}
+            </div>
+        </div>
+    )
+};
+
+export default Post;
