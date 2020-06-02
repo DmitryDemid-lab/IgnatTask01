58655a14d5358f6a2ee1f7c14d9dfff436cbf543
DmitryDemid <dimon.demid15@gmail.com>
2020-06-02 19:15:31 -0400

    corrections in Post

--- src/Components/TelegramMessage/TelegramMessage.tsx
+++ src/Components/TelegramMessage/TelegramMessage.tsx
@@ -4,8 +4,10 @@ import s from './TelegramMessage.module.css';
 const Post = () => {
     return (
         <div className={s.post}>
-            <Avatar/>
-            <Message name={"Dmitry"} text={"Hi, how'r u doing?"}/>
+            <div className={s.messageBlock}><Avatar/>
+                <Message name={"Dmitry"}
+                         text={"Hi, how'r u doing? One of the most common problems that I run into when using Redux is trying to figure out why an action is not being captured by a reducer. For someone just getting starting with Redux, debugging this issue can be especially overwhelming because of how Redux manages data flow. So before you start pouring over configuration code, or the logic contained in your action creators and reducers, please, make sure your action types are defined and spelled correctly."}/>
+            </div>
         </div>
     )
 };
