1a2b17b1a4f80a0c458c17c4c21aa7968926fc1b
DmitryDemid <dimon.demid15@gmail.com>
2020-06-05 22:27:28 -0400

    Stiles added

--- src/Components/Affairs/Affairs.tsx
+++ src/Components/Affairs/Affairs.tsx
@@ -17,19 +17,21 @@ type newAffairsType = {
 
 function Affairs(props: newAffairsType) {
 
-    let newAffairs = props.affairs.map(a => <li key={a.id}>
-        <span> <b>name: </b> {a.name}</span>
-        <span> <b>priority:</b> {a.priority}</span>
-        <button  onClick={() => props.removeAffair(a.id)}>x</button>
+    let newAffairs = props.affairs.map(a => <li className={s.textRow} key={a.id}>
+        <span className={s.affair}> <span className={s.constRow}>Name: </span> {a.name}</span>
+        <span className={s.affair}> <span className={s.constRow}>Priority:</span> {a.priority}</span>
+        <button className={s.btnDelete}  onClick={() => props.removeAffair(a.id)}>x</button>
     </li>)
 
     return (
         <div>
-            <ul>{newAffairs}</ul>
-            <button className={s.btn} onClick={() => props.changeFilter("all")}>All</button>
-            <button className={s.btn} onClick={() => props.changeFilter("high")}>High</button>
-            <button className={s.btn} onClick={() => props.changeFilter("middle")}>Middle</button>
-            <button className={s.btn} onClick={() => props.changeFilter("low")}>Low</button>
+            <ul >{newAffairs}</ul>
+            <div className={s.btnRow}>
+                <button className={s.btn} onClick={() => props.changeFilter("all")}>ALL</button>
+                <button className={s.btn} onClick={() => props.changeFilter("high")}>HIGH</button>
+                <button className={s.btn} onClick={() => props.changeFilter("middle")}>MIDDLE</button>
+                <button className={s.btn} onClick={() => props.changeFilter("low")}>LOW</button>
+            </div>
         </div>
 
     )
