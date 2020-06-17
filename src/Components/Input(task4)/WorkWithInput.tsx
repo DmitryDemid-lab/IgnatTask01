5427097d78ed1e05b5c9ecf59a643d00b0c7d912
DmitryDemid <dimon.demid15@gmail.com>
2020-06-17 18:16:45 -0400

    Adding new component WorkWithInput

---
+++ src/Components/Input(task4)/WorkWithInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './WorkWithInput.module.css';
+
+function WorkWithInput() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+};
+
+export default WorkWithInput;
