56defd505b78f9fa460457e78627789266061ac4
DmitryDemid <dimon.demid15@gmail.com>
2020-06-25 21:48:16 -0400

    Updates

---
+++ precache-manifest.84f0adc349e7525dd60668e9373bf6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cb9bd37fef41ca19fe575ba923b93b",
+    "url": "/YIgnatTaskY/index.html"
+  },
+  {
+    "revision": "b1c360ae707a0b671410",
+    "url": "/YIgnatTaskY/static/css/main.d838bc16.chunk.css"
+  },
+  {
+    "revision": "ec2eaaf062ce30400f0f",
+    "url": "/YIgnatTaskY/static/js/2.1d9d90ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YIgnatTaskY/static/js/2.1d9d90ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c360ae707a0b671410",
+    "url": "/YIgnatTaskY/static/js/main.090f78c2.chunk.js"
+  },
+  {
+    "revision": "72fc245be9fa487b757e",
+    "url": "/YIgnatTaskY/static/js/runtime-main.12bf2dda.js"
+  }
+]);
