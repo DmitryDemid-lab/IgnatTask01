690b7de17663e251944005c6cd363e83cb309e96
DmitryDemid <dimon.demid15@gmail.com>
2020-06-30 22:27:35 -0400

    Using new features

--- src/AppPages/Junior/Junior.tsx
+++ src/AppPages/Junior/Junior.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import s from './Junior.module.css'
+import EditableSpan from "../../Common/EditableSpan/EditableSpan";
+
+type StateType = {
+    x: string
+    y: number
+}
 
 function Junior() {
     return (
         <div className={s.Junior}>
             <h1>Junior</h1>
+            <EditableSpan title={"Hello"}/>
         </div>
     )
 };
