bb0899ba2f150ddb44369225551b024001b8ba6b
DmitryDemid <dimon.demid15@gmail.com>
2020-07-24 23:03:26 -0400

    Creating reducer

--- src/reducer/homeWorkReducer.ts
+++ src/reducer/homeWorkReducer.ts
@@ -4,12 +4,41 @@ export type StateType = {
     age: number
 }
 
-export type ActionType = {
-    type: string,
-    payload: any
+export type SortStateActionType = {
+    type: 'SORT'
+    sortType: string
 }
 
-export const hwReducer = (state: Array<StateType>, action:ActionType ) => {
+export type CheckStateActionType = {
+    type: 'CHECK'
+    minAge: number
+}
 
-    return [];
+export type ActionType = SortStateActionType | CheckStateActionType
+
+export const hwReducer = (state: Array<StateType>, action: ActionType) => {
+    switch (action.type) {
+        case "SORT": {
+            let stateCopy = [...state]
+            if (action.sortType === 'up') {
+                return stateCopy.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+            } else if (action.sortType === 'down') {
+                return stateCopy.sort((a, b) => (a.name < b.name) ? 1 : ((b.name < a.name) ? -1 : 0))
+            }
+            return stateCopy;
+        }
+        case "CHECK": {
+            let stateCopy = [...state]
+            return stateCopy.filter(p => p.age >= action.minAge)
+        }
+        default:
+            throw new Error("Can't find this type")
+    }
 };
+
+export const sortStateAC = (sortType: string): SortStateActionType => {
+    return {type: "SORT", sortType}
+}
+export const checkStateAC = (minAge: number): CheckStateActionType => {
+    return {type: "CHECK", minAge}
+}
