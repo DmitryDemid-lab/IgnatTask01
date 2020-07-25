125d6a24bf14b52c12244ec6c0b60306ef2be23a
DmitryDemid <dimon.demid15@gmail.com>
2020-07-24 23:03:35 -0400

    Testing reducer

--- src/reducer/homeWorkReducer.test.ts
+++ src/reducer/homeWorkReducer.test.ts
@@ -1,18 +1,49 @@
-import {hwReducer, StateType} from "./homeWorkReducer";
+import {checkStateAC, hwReducer, sortStateAC, StateType} from "./homeWorkReducer";
 
 test ('array should be sorted by names up', () => {
 
     const startState: Array<StateType> = [
         {id: '1', name: 'Dmitry', age: 22},
-        {id: '2', name: 'Alex', age: 32}
+        {id: '2', name: 'Alex', age: 32},
+        {id: '3', name: 'Max', age: 12}
     ]
 
-    const action = {
-        type: 'SORT',
-        payload: 'up'
-    }
+    const action = sortStateAC("up")
 
     const resultState = hwReducer(startState, action)
 
-    expect(resultState[0]).toBe(resultState[1]);
+    expect(resultState[0].id).toBe('2');
+    expect(resultState[1].id).toBe('1');
+});
+
+test ('array should be sorted by names down', () => {
+
+    const startState: Array<StateType> = [
+        {id: '1', name: 'Dmitry', age: 22},
+        {id: '2', name: 'Alex', age: 32},
+        {id: '3', name: 'Max', age: 12}
+    ]
+
+    const action = sortStateAC("down")
+
+    const resultState = hwReducer(startState, action)
+
+    expect(resultState[0].id).toBe('3');
+    expect(resultState[1].id).toBe('1');
+});
+
+test ('in result array should be people older than we need', () => {
+
+    const startState: Array<StateType> = [
+        {id: '1', name: 'Dmitry', age: 22},
+        {id: '2', name: 'Alex', age: 32},
+        {id: '3', name: 'Max', age: 12}
+    ]
+
+    const action = checkStateAC(18)
+
+    const resultState = hwReducer(startState, action)
+
+    expect(resultState.length).toBe(2);
+    expect(resultState.every (p => p.id !== '3')).toBeTruthy()
 });
