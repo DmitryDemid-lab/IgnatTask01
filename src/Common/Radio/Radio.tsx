7c5b2042d5f02b65d9df08bf443ed4c9c823d90b
DmitryDemid <dimon.demid15@gmail.com>
2020-07-24 23:04:56 -0400

    Refactoring new common components

--- src/Common/Radio/Radio.tsx
+++ src/Common/Radio/Radio.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from "./Radio.module.css"
 
-type OptionsType = {
+type RadioGroupType = {
+    id: number
     groupName: string
-    title: Array<string>
+    title: string
 }
 
 type RadioType = {
-    radioGroup: OptionsType
+    radioGroup: Array<RadioGroupType>
 }
 
 function Radio(props: RadioType) {
+    const [editMode, setMode] = useState<boolean>(true)
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        alert('want to change')
+        // setTitle(e.currentTarget.value)
+    }
+
     return (
-        <div className={s.Radio}>
-            {props.radioGroup.title.map((r, i) => <p key={i}><input type="radio" name={props.radioGroup.groupName}/>{r}</p>)}
-        </div>
+        <>
+            {props.radioGroup.map(r => {
+                   return !editMode
+                        ? <div key={r.id} onDoubleClick={() => {
+                            setMode(true)
+                        }}>
+                            <input
+                                type={'radio'}
+                                name={r.groupName}
+                            />
+                            <label>{r.title}</label>
+                        </div>
+                        : <input
+                            key={r.id}
+                            type={'text'}
+                            value={r.title}
+                            onBlur={() => {
+                                setMode(false)
+                            }}
+                            autoFocus
+                            onChange={onChangeHandler}
+                />
+                }
+             )}
+        </>
+
     )
 };
 
--- src/Common/Select/Select.tsx
+++ src/Common/Select/Select.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from "./Select.module.css"
 
 type SelectType = {
     optionsArray: Array<string>
+
 }
 
 function Select(props: SelectType) {
+    const onChangeH = (e: ChangeEvent<HTMLSelectElement>) => {
+        console.log(e.currentTarget.value)
+    }
+
     return (
-        <select className={s.Select}>
-            {props.optionsArray.map((op, i) => <option key={i}>{op}</option>)}
+        <select className={s.Select} onChange={onChangeH}>
+            {props.optionsArray.map((op, i) => <option key={i} value={"sas"}>{op}</option>)}
         </select>
     )
 };
