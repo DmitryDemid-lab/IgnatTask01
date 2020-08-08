cc48b378e366deccfc3beb320c4a235fa70e1098
DmitryDemid <dimon.demid15@gmail.com>
2020-08-08 12:38:02 -0400

    Creating new common component Range

---
+++ src/Common/Range/Range.tsx
@@ -0,0 +1,29 @@
+import React, {ChangeEvent} from 'react';
+import s from "./Range.module.css"
+
+type RangeType ={
+    title: string
+    minVal: number
+    maxVal: number
+    onRangeChange: (currentValue: number) => void
+    value: number
+}
+
+export const Range = React.memo((props:RangeType) => {
+
+    const onRangeChange = (e: ChangeEvent<HTMLInputElement>) => {
+        props.onRangeChange(+e.currentTarget.value)
+        console.log(`VALUE INSIDE COMPONENT ${e.currentTarget.value}`)
+    }
+
+    return <div className={s.Range}>
+            <input
+                type="range"
+                min={props.minVal}
+                max={props.maxVal}
+                onChange={onRangeChange}
+                value={props.value}
+            />
+            <label>{props.title}</label>
+        </div>
+});
