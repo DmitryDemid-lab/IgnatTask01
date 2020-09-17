fef8cb71dd33731487dcb7a2c84aa0cc947a2124
DmitryDemid <dimon.demid15@gmail.com>
2020-09-17 16:23:11 -0400

    creating requsestApi to send checkbox value and showing result in JuniorPlus page

--- src/AppPages/Junior+/JuniorPlus.tsx
+++ src/AppPages/Junior+/JuniorPlus.tsx
@@ -1,9 +1,12 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import s from './JuniorPlus.module.css'
 import {Select} from '../../Common/Select/Select';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../redux/store";
 import {setTheme, themeType} from "../../redux/reducers/themeSwitchReducer/themeSwitchReducer";
+import ButtonNya from "../../Components/Input(task4)/ButtonNya/ButtonNya";
+import {RequestsAPI} from "../../Api/api";
+import {log} from "util";
 
 export default function JuniorPlus() {
     const theme = useSelector<AppRootStateType>(state => state.theme.theme)
@@ -12,6 +15,9 @@ export default function JuniorPlus() {
             className={`${s.JuniorPlus}  ${theme === 'dark' && s.bgDark} ${theme === 'light' && s.bgLight} ${theme === 'color' && s.bgColor}`}>
             <h1>JuniorPlus</h1>
             <ThemeSwitcher/>
+            <br/><hr/>
+            <Request/>
+            <br/><hr/>
         </div>
     )
 };
@@ -28,5 +34,27 @@ export const ThemeSwitcher = React.memo(() => {
 
     return <div>
         <Select value={theme} onSelectChangeHandler={onThemeChange} optionsArray={themesArr}/>
+    </div>
+})
+
+export const Request = React.memo(() => {
+    const [state, setState] = useState(false)
+    const [res, setRes] = useState('')
+    const [error, setError] = useState('')
+
+    const onSendHandler = () => {
+        RequestsAPI.setSuccess(state)
+            .then( res => setRes(res.data.errorText))
+            .catch(error => setError(error.message))
+        setError('')
+        setRes('')
+    }
+
+    return <div>
+        <input type={"checkbox"} onClick={e=>setState(e.currentTarget.checked)}/>
+        <ButtonNya onClick={onSendHandler}>send</ButtonNya>
+        <div>
+            <p>{error ? error : res}</p>
+        </div>
     </div>
 })
